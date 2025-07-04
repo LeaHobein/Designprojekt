4e25d314cb819caf06bd4107fb1e7cefeb01dbba
LOEA <lea.hobein@gmail.com>
2025-07-04 16:12:49 +0200

    neue Farben

--- server.js
+++ server.js
@@ -120,7 +120,7 @@ function findAndStartTshark() {
   // Wir tragen diese Nummer hier fest ein, um alle Erkennungsfehler zu umgehen.
 
   //Kristina: INTERFACE_ID = '4'; path: 'COM5',
-  //Lea: INTERFACE_ID = '5'; path: 'COM4',
+  //Lea: INTERFACE_ID = '5'; path: 'COM4', beim eigenen `COM5`
   //Krystyna: INTERFACE_ID = '5'; path: 'COM5',
 
   const INTERFACE_ID = '5'; 
